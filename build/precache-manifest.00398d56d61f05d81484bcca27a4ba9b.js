f8d73de63527b3f226e1d53af4419365bf93a0ab
SubroBlack <dahalsubodh8@gmail.com>
2020-09-26 00:10:00 +0300

    AppUpdate

---
+++ build/precache-manifest.00398d56d61f05d81484bcca27a4ba9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2590cc00e91ced174fd8",
+    "url": "/static/css/main.027995f3.chunk.css"
+  },
+  {
+    "revision": "2590cc00e91ced174fd8",
+    "url": "/static/js/main.8f494ca1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3ca5bc8da92203678565",
+    "url": "/static/js/2.1d41e463.chunk.js"
+  },
+  {
+    "revision": "75a6e48e7a2ef47aea4f8dccf27d13b3",
+    "url": "/index.html"
+  }
+];
