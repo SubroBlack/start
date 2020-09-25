6500c132cf6611fa0d58529f03709908ec6da8f9
SubroBlack <dahalsubodh8@gmail.com>
2020-09-25 22:50:26 +0300

    AppUpdate

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const app = express();
 
 app.use(cors());
 
-app.use("/", express.static("build"));
+app.use("/app", express.static("build"));
 app.get("/", express.static("build"));
+app.get("/trial", (req, res) => {
+  res.send("Start Now");
+});
 //app.use("/*", express.static("build"));
 
 app.listen(process.env.PORT || 3001, () => {
