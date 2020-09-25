c58dab0674d355bc11ef177f705313653531b5bc
SubroBlack <dahalsubodh8@gmail.com>
2020-09-26 00:01:56 +0300

    AppUpdate

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ app.use(cors());
 
 app.use("/", express.static("build"));
 
+app.use(express.json());
+
 app.get("/trial", (req, res) => {
   res.send("Start Now");
 });
 
-//app.use("/*", express.static("build"));
+app.use("/*", express.static("build"));
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Serve running on Port ${process.env.PORT}`);
