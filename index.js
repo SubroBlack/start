78b1f93c9cbb7357967e977be3dddd446c8c5431
SubroBlack <dahalsubodh8@gmail.com>
2020-09-25 22:59:36 +0300

    uibuild

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express();
 
 app.use(cors());
 
-app.use("/app", express.static("build"));
-app.get("/", express.static("build"));
+app.use("/", express.static("build"));
+app.get("/home", express.static("build"));
 app.get("/trial", (req, res) => {
   res.send("Start Now");
 });
-//app.use("/*", express.static("build"));
+app.use("/*", express.static("build"));
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Serve running on Port ${process.env.PORT}`);
