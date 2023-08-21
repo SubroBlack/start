d58d3b6e13d27b9814f2f34266d7054b7f499703
SubroBlack <dahalsubodh8@gmail.com>
2023-08-21 16:44:37 +0300

    Trying to run app using node 16

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const app = express();
 
 app.use(cors());
 
-// Just serving the Static Build Folder and nothing else
-//app.use("/", express.static("build"));
-//app.use("/*", express.static("build"));
-app.get('/', (req, res) => {
+// Test URL test
+app.get('/hi', (req, res) => {
   res.send(`Hello World! from ${process.env.PORT}, aaaand thats it. I gotta go, I'm busy. I have an interview coming up. Take Care :D`)
 })
+
+// Just serving the Static Build Folder and nothing else
+app.use("/", express.static("build"));
+//app.use("/*", express.static("build"));
+
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server running on Port ${process.env.PORT || 3001}`);
