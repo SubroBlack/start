d13b97d9e09e498b9b3f8c32b09e78a5c70886a7
SubroBlack <dahalsubodh8@gmail.com>
2023-08-21 16:19:00 +0300

    Todo app aint working, changed to show text and do GitHub Actions

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(cors());
 
 // Just serving the Static Build Folder and nothing else
-app.use("/", express.static("build"));
-
-app.use("/*", express.static("build"));
+//app.use("/", express.static("build"));
+//app.use("/*", express.static("build"));
+app.get('/', (req, res) => {
+  res.send(`Hello World! from ${process.env.PORT}, aaaand thats it. I gotta go, I'm busy. I have an interview coming up. Take Care :D`)
+})
 
 app.listen(process.env.PORT || 3001, () => {
-  console.log(`Serve running on Port ${process.env.PORT || 3001}`);
+  console.log(`Server running on Port ${process.env.PORT || 3001}`);
 });
